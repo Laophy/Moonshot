e4f75bf9ebfa66570cd215f2b7b039b7caa748b8
JordanPessman <jpessman22@gmail.com>
2021-04-15 22:21:49 -0500

    Main

---
+++ main.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.on('ready', () => {
+    console.log("Bot logging in.")
+})
+
+client.login(process.env.BOT_TOKEN);
