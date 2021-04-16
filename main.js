2da5dedfaa1f09123ebe988a440df1f199958597
JordanPessman <JordanPessman@users.noreply.github.com>
2021-04-16 14:22:40 -0500

    Updated commands !help !commands !moonshot

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 const BOT_PREFIX = "!";
 
 client.on("ready", () => {
-  console.log("Bot logging in.");
+  console.log("Moonshot bot has logged in.");
 });
 
 client.on("message", (msg) => {
@@ -16,6 +16,33 @@ client.on("message", (msg) => {
       "Join the test flight!: http://testflight.apple.com/join/LINhIVOP"
     );
     //msg.member.roles.add("832459385292062763");
+  }
+  if (
+    msg.content === `${BOT_PREFIX}help` ||
+    msg.content === `${BOT_PREFIX}commands`
+  ) {
+    //Do something
+    const helpEmbed = new Discord.MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("Moonstot Help")
+      .setDescription(
+        "Welcome to the Moonshot Discord! Here you will find the list of commands available to you!"
+      )
+      .addFields(
+        { name: "Bot Help", value: "!help, !commands", inline: true },
+        { name: "Test Flight", value: "!moonshot", inline: true }
+      )
+      .setThumbnail("https://i.imgur.com/2OBXsKZ.png");
+    msg.channel.send(helpEmbed);
+  }
+  if (
+    msg.content === `moonshot` ||
+    msg.content === `great app` ||
+    msg.content === `cool idea` ||
+    msg.content === `moonshot is awesome` ||
+    msg.content === `good idea`
+  ) {
+    msg.react("❤️");
   }
 });
 
