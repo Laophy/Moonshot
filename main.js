a14c438bc62207e202e6b845ab29a459e73f888c
JordanPessman <JordanPessman@users.noreply.github.com>
2021-04-16 13:43:45 -0500

    init

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const BOT_PREFIX = "!"
+const BOT_PREFIX = "!";
 
-client.on('ready', () => {
-    console.log("Bot logging in.")
-})
+client.on("ready", () => {
+  console.log("Bot logging in.");
+});
 
-
-client.on('message', msg =>{
-    if(msg.content === `${BOT_PREFIX}moonshot`){
-        msg.react("❤️");
-        msg.reply("Join the test flight!: http://testflight.apple.com/join/LINhIVOP");
-        msg.member.roles.add("832459385292062763");
-    }
-})
-
-
+client.on("message", (msg) => {
+  if (msg.content === `${BOT_PREFIX}moonshot`) {
+    msg.react("❤️");
+    msg.reply(
+      "Join the test flight!: http://testflight.apple.com/join/LINhIVOP"
+    );
+    //msg.member.roles.add("832459385292062763");
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
